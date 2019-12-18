cc7c6c3934c43036fd075ae20148437d624b12c8
Deynega Andrey <andrtomsk@narod.ru>
2019-12-18 13:10:30 +0700

    improve calendar

--- src/components/elements/input-calendar/input-calendar.js
+++ src/components/elements/input-calendar/input-calendar.js
@@ -2,23 +2,32 @@ import "../label/label";
 import "./input-calendar.scss";
 import "../../common/datepicker-here/datepicker-here";
 
+class DatePickerHere {
+  constructor() {
+    this.init();
+  }
+  init = () => {
+    $("#start").datepicker({
+      multipleDates: 2,
+      range: true,
+      multipleDatesSeparator: " - "
+    });
+
+    $("#start").datepicker({
+      onSelect: function(fd, d, picker) {
+        $("#start").val(fd.split("-")[0]);
+        $("#end").val(fd.split("-")[1]);
+      }
+    });
+
+    $("#end").click(function(e) {
+      $("#start")
+        .data("datepicker")
+        .show();
+    });
+  };
+}
+
 $(document).ready(function() {
-  $("#start").datepicker({
-    multipleDates: 2,
-    range: true,
-    multipleDatesSeparator: " - "
-  });
-
-  $("#start").datepicker({
-    onSelect: function(fd, d, picker) {
-      $("#start").val(fd.split("-")[0]);
-      $("#end").val(fd.split("-")[1]);
-    }
-  });
-
-  $("#end").click(function(e) {
-    $("#start")
-      .data("datepicker")
-      .show();
-  });
+  new DatePickerHere();
 });
