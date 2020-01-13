c86db67d34fdbec9fc27d0c38ce959612d22c9a7
Deynega Andrey <andrtomsk@narod.ru>
2020-01-13 18:28:15 +0700

    improve classes names in mixin block

--- webpack.config.js
+++ webpack.config.js
@@ -92,7 +92,7 @@ module.exports = {
       template: './src/sign-in.pug',
     }),
     new HTMLWebpackPlugin({
-      template: './src/demo.pug',
+      template: './src/colors-type.pug',
     }),
     new CopyWebpackPlugin([
       { from: './src/assets/img', to: './assets/img' },
