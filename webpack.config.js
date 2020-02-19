1f2c394afb440ef6b6bc1ff808430c5c8633cd05
Deynega Andrey <andrtomsk@narod.ru>
2020-02-19 17:58:59 +0700

    fixed webpack for presentation

--- webpack.config.js
+++ webpack.config.js
@@ -92,7 +92,7 @@ module.exports = {
       template: './src/sign-in.pug',
     }),
     new HTMLWebpackPlugin({
-      template: './src/headers-footers.pug',
+      template: './src/demo.pug',
     }),
     new CopyWebpackPlugin([
       { from: './src/assets/img', to: './assets/img' },
