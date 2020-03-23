6d0d988c7f6c99b408f92bd515fe72d2f16ac600
Deynega Andrey <andrtomsk@narod.ru>
2020-03-23 14:01:42 +0700

    changed webpack for check

--- webpack.config.js
+++ webpack.config.js
@@ -108,7 +108,7 @@ module.exports = {
       template: './src/sign-in.pug',
     }),
     new HTMLWebpackPlugin({
-      template: './src/cards.pug',
+      template: './src/demo.pug',
     }),
     new CopyWebpackPlugin([
       { from: './src/assets/img', to: './assets/img' },
