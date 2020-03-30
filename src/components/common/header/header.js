40bc1959105f0bbdefca098bc321702b678de1cc
Deynega Andrey <andrtomsk@narod.ru>
2020-03-30 16:23:50 +0700

    issue #11 made center position of header

--- src/components/common/header/header.js
+++ src/components/common/header/header.js
@@ -12,12 +12,14 @@ class Header {
   findDom() {
     this.$burger = this.$header.find('.js-header__burger');
     this.$container = this.$header.find('.js-header__container');
+    this.$a = this.$header.find('.js-header__a');
   }
 
   addEventListeners() {
     this.$burger.on('click', () => {
       this.$container.toggleClass('header__container_hide');
-      this.$burger.toggleClass('change');
+      this.$burger.toggleClass('change header__bottom');
+      this.$a.toggleClass('header__bottom');
     });
   }
 }
